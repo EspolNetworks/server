facff3fd22c8533ea5d54d1b1da9e4b4a3e127b9
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2024-07-28 11:34:16 -0500

    WIP: calculate ipv4header + headerC

--- c/tcp.c
+++ c/tcp.c
@@ -4,6 +4,7 @@ ethernet_frame create_ethernet_frame(unsigned char payload[MAX_PAYLOAD_LEN])
 {
     ethernet_frame frame;
 
+    // TODO: set the real source and destination MAC addresses
     frame.source_mac[0] = 0x00;
     frame.source_mac[1] = 0x00;
     frame.source_mac[2] = 0x00;
@@ -36,6 +37,61 @@ ethernet_frame create_ethernet_frame(unsigned char payload[MAX_PAYLOAD_LEN])
     return frame;
 }
 
-ipv4_header create_ipv4_header() {
-    
+ipv4_header create_ipv4_header()
+{
+
+    ipv4_header header;
+    header.version[0] = 0x04;
+    header.type_service[0] = 0x00;
+    header.total_length[0] = 0x00;
+    header.total_length[1] = 0x14;
+    header.identification[0] = 0x00;
+    header.identification[1] = 0x00;
+    header.flags[0] = 0x40;
+    header.flags[1] = 0x00;
+    header.time_to_live[0] = 0x40;
+    header.protocol[0] = 0x06;
+    header.header_checksum[0] = 0x00;
+    header.header_checksum[1] = 0x00;
+    // TODO: set the real source and destination IP addresses
+    header.source_ip[0] = 0x7F;
+    header.source_ip[1] = 0x00;
+    header.source_ip[2] = 0x00;
+    header.source_ip[3] = 0x01;
+    header.dest_ip[0] = 0x7F;
+    header.dest_ip[1] = 0x00;
+    header.dest_ip[2] = 0x00;
+    header.dest_ip[3] = 0x01;
+
+    short checksum = calculate_ipv4_checksum(&header);
+
+    return header;
+}
+
+short calculate_ipv4_checksum(ipv4_header *header)
+{
+    unsigned short checksum_in_words[10];
+    checksum_in_words[0] = (header->version[0] << 8) + header->type_service[0];
+    checksum_in_words[1] = (header->total_length[0] << 8) + header->total_length[1];
+    checksum_in_words[2] = (header->identification[0] << 8) + header->identification[1];
+    checksum_in_words[3] = (header->flags[0] << 8) + header->flags[1];
+    checksum_in_words[4] = (header->time_to_live[0] << 8) + header->protocol[0];
+    checksum_in_words[5] = (header->header_checksum[0] << 8) + header->header_checksum[1];
+    checksum_in_words[6] = (header->source_ip[0] << 8) + header->source_ip[1];
+    checksum_in_words[7] = (header->source_ip[2] << 8) + header->source_ip[3];
+    checksum_in_words[8] = (header->dest_ip[0] << 8) + header->dest_ip[1];
+    checksum_in_words[9] = (header->dest_ip[2] << 8) + header->dest_ip[3];
+
+    unsigned int checksum = 0;
+
+    for (int i = 0; i < 10; i++)
+    {
+        checksum += checksum_in_words[i];
+    }
+
+    if (sizeof(checksum) > 2)
+    {
+    }
+
+    return checksum;
 }
--- c/tcp.h
+++ c/tcp.h
@@ -8,25 +8,25 @@
 // https://en.wikipedia.org/wiki/Ethernet_frame
 typedef struct ethernet // 18 bytes + payload
 {
-    unsigned char dest_mac[6];              
-    unsigned char source_mac[6];            
-    unsigned char type[2];                  
-    unsigned char payload[MAX_PAYLOAD_LEN]; 
-    unsigned char fcs[4];                   
+    unsigned char dest_mac[6];
+    unsigned char source_mac[6];
+    unsigned char type[2];
+    unsigned char payload[MAX_PAYLOAD_LEN];
+    unsigned char fcs[4];
 } ethernet_frame;
 
 typedef struct packet_header // 20 bytes
 {
-    unsigned char version[1];         
-    unsigned char type_service[1];    
-    unsigned char total_length[2];    
-    unsigned char identification[2];  
-    unsigned char flags[2];           
-    unsigned char time_to_live[1];    
-    unsigned char protocol[1];        
-    unsigned char header_checksum[2]; 
-    unsigned char source_ip[4];       
-    unsigned char dest_ip[4];         
+    unsigned char version[1];
+    unsigned char type_service[1];
+    unsigned char total_length[2];
+    unsigned char identification[2];
+    unsigned char flags[2];
+    unsigned char time_to_live[1];
+    unsigned char protocol[1];
+    unsigned char header_checksum[2];
+    unsigned char source_ip[4];
+    unsigned char dest_ip[4];
 } ipv4_header;
 
 typedef struct packet // 38 bytes + data
@@ -37,16 +37,16 @@ typedef struct packet // 38 bytes + data
 
 typedef struct tcp // 24 bytes
 {
-    unsigned char source_port[2];     
-    unsigned char dest_port[2];       
-    unsigned char sequence_number[4]; 
-    unsigned char ack_number[4];      
-    unsigned char data_offset[2];     
-    unsigned char reserved[2];        
-    unsigned char flags[2];           
-    unsigned char window_size[2];     
-    unsigned char checksum[2];        
-    unsigned char urgent_pointer[2];  
+    unsigned char source_port[2];
+    unsigned char dest_port[2];
+    unsigned char sequence_number[4];
+    unsigned char ack_number[4];
+    unsigned char data_offset[2];
+    unsigned char reserved[2];
+    unsigned char flags[2];
+    unsigned char window_size[2];
+    unsigned char checksum[2];
+    unsigned char urgent_pointer[2];
 } tcp_header;
 
 typedef struct tcp_packet // 62 bytes + data
@@ -57,5 +57,6 @@ typedef struct tcp_packet // 62 bytes + data
 
 ethernet_frame create_ethernet_frame(unsigned char payload[MAX_PAYLOAD_LEN]);
 ipv4_header create_ipv4_header();
+short calculate_ipv4_checksum(ipv4_header *header);
 
 #endif // TCP_H
